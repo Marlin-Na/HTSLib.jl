9f1ce1dbd06de3552e0e58d1bb15c151d6cab1b0
Jialin Ma <jma@broadinstitute.org>
2021-06-21 13:25:17 +0000

    Iteration interface of HTSIOWrapper

--- src/htsopen.jl
+++ src/htsopen.jl
@@ -167,3 +167,22 @@ end
 
 # Iterator
 # --------
+
+function Base.iterate(hf::HTSIOWrapper, state::Missing)
+    record = BamRecord()
+    try
+        read!(hf, record)
+    catch err
+        if err isa EOFError
+            return nothing
+        else
+            rethrow()
+        end
+    end
+    return (record, state)
+end
+
+function Base.iterate(hf::HTSIOWrapper)
+    state = missing
+    iterate(hf, state)
+end
